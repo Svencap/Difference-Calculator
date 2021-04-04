d9a44a5f9ec97f590fecb60008c5e0287c017148
Ayzek <Ayzekmanduar@mail.ru>
2021-04-05 03:04:35 +0900

    add test yaml

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@jest/globals';
+
+import { getPath, parser } from '../src/getPath';
+
+// import getData from '../src/getData';
+
+import getdiff from '../gendiffJson';
+
+test('gendiffJson', () => {
+  const pathBefore = getPath('file1.json');
+  const pathAfter = getPath('file2.json');
+  const dataBeforeFile = parser(pathBefore);
+  const dataAfterFile = parser(pathAfter);
+  const expectDiff = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+  expect(getdiff(dataBeforeFile, dataAfterFile)).toBe(expectDiff);
+});
+
+test('gendiffYml', () => {
+  const pathBefore = getPath('file1.yml');
+  const pathAfter = getPath('file2.yml');
+  const expectDiff = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+  expect(getdiff(parser(pathBefore), parser(pathAfter))).toEqual(expectDiff);
+});
