c20db40db24d6d7a79377859ac7fb9a728fe74a0
Ayzek <Ayzekmanduar@mail.ru>
2021-07-08 02:07:42 +0900

    add formater

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ import { parser } from '../src/getPath.js';
 
 // import getData from '../src/getData.js';
 
-import getdiff from '../gendiffFiles.js';
+import { diff, formater } from '../gendiffFiles.js';
 
 const program = new Command();
 
@@ -17,13 +17,9 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    // const pathToFile1 = getPath(filepath1);
-    // const pathToFile2 = getPath(filepath2);
-    // const readingFile1 = getData(pathToFile1);
-    // const readingFile2 = getData(pathToFile2);
-    // console.log(parser(filepath1));
-    // console.log(parser(filepath2));
-    console.log(getdiff(parser(filepath1), parser(filepath2)));
-    // console.log(getdiff(parser(filepath1), parser(filepath2)));
+    const treeAst = diff(parser(filepath1), parser(filepath2));
+    // console.log(treeAst);
+    // console.log(formater(treeAst));
+    console.log(formater(treeAst));
   })
   .parse();
--- gendiffFiles.js
+++ gendiffFiles.js
@@ -1,31 +1,67 @@
 import _ from 'lodash';
 
-const getdiff = (file1, file2) => {
-  let diff = '{';
-  const keysFile1 = Object.keys(file1);
-  const keysFile2 = Object.keys(file2);
-  const keys = keysFile1.concat(keysFile2).sort();
-  const result = _.keyBy(keys);
-  Object.entries(result).forEach(([key]) => {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] === file2[key]) diff += `\n    ${key}: ${file1[key]}`;
-      else diff += `\n  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
+/* eslint object-curly-newline: ["error", "never"] */
+const diff = (file1, file2) => {
+  const buildAst = [];
+  const uniqKeys = _.sortBy(_.uniq([..._.keys(file1), ..._.keys(file2)]));
+  uniqKeys.forEach((key) => {
+    if (typeof file1[key] === 'object' && typeof file2[key] === 'object') {
+      buildAst.push({ name: key, children: diff(file1[key], file2[key]) });
+    } else if (_.has(file1, key) && _.has(file2, key)) {
+      if (file1[key] === file2[key]) {
+        buildAst.push({ name: key, value: file1[key], status: 'unchanged' });
+      } else if (typeof file1[key] === 'object') {
+        buildAst.push({ name: key, children: diff(file1[key], file1[key]), newValue: file2[key], status: 'changed' });
+      } else {
+        buildAst.push({ name: key, oldValue: file1[key], newValue: file2[key], status: 'changed' });
+      }
+    } else if (!_.has(file1, key) && _.has(file2, key)) {
+      if (typeof file2[key] === 'object') {
+        buildAst.push({ name: key, children: diff(file2[key], file2[key]), status: 'added' });
+      } else buildAst.push({ name: key, value: file2[key], status: 'added' });
+    } else if (_.has(file1, key) && !_.has(file2, key)) {
+      if (typeof file1[key] === 'object') {
+        buildAst.push({ name: key, children: diff(file1[key], file1[key]), status: 'deleted' });
+      } else buildAst.push({ name: key, value: file1[key], status: 'deleted' });
     }
-    if (_.has(file1, key) && !_.has(file2, key)) diff += `\n  - ${key}: ${file1[key]}`;
-    else if (!_.has(file1, key) && _.has(file2, key)) diff += `\n  + ${key}: ${file2[key]}`;
   });
-  diff += '\n}';
-  return diff;
+  return buildAst;
 };
 
-export default getdiff;
-
-/**
- * for (const [key] of Object.entries(result)) {
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] === file2[key]) diff += `\n    ${key}: ${file1[key]}`;
-      else diff += `\n  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
+const formater = (tree) => {
+  const space = ' ';
+  // let countBracket = 0;
+  const iter = (node, depth) => {
+    const result = [];
+    if (depth === 2) {
+      result.push('{');
     }
-    if (_.has(file1, key) && !_.has(file2, key)) diff += `\n  - ${key}: ${file1[key]}`;
-    else if (!_.has(file1, key) && _.has(file2, key)) diff += `\n  + ${key}: ${file2[key]}`;
-} */
+    node.forEach((item) => {
+      const { name, value, oldValue, newValue, status, children } = item;
+      if (status === 'unchanged') {
+        result.push(`${space.repeat(depth)}  ${name}: ${value}`);
+      } else if (status === 'added') {
+        if (children !== undefined) {
+          result.push(`${space.repeat(depth)}+ ${name}: {\n${iter(children, depth + 4)} \n${space.repeat(depth)}  }`);
+        } else result.push(`${space.repeat(depth)}+ ${name}: ${value}`);
+      } else if (status === 'deleted') {
+        if (children !== undefined) {
+          result.push(`${space.repeat(depth)}- ${name}: {\n${iter(children, depth + 4)} \n${space.repeat(depth)}  }`);
+        } else result.push(`${space.repeat(depth)}- ${name}: ${value}`);
+      } else if (status === 'changed') {
+        if (children !== undefined) {
+          result.push(`${space.repeat(depth)}- ${name}: {\n${iter(children, depth + 4)}\n${space.repeat(depth)}  } \n${space.repeat(depth)}+ ${name}: ${newValue}`);
+        } else result.push(`${space.repeat(depth)}- ${name}: ${oldValue}\n${space.repeat(depth)}+ ${name}: ${newValue}`);
+      } else if (children !== undefined) {
+        result.push(` ${space.repeat(depth)} ${name}: {\n${iter(children, depth + 4)} \n${space.repeat(depth)}  }`);
+      }
+    });
+    if (depth === 2) {
+      result.push('}');
+    }
+    return result.join('\n');
+  };
+  return iter(tree, 2);
+};
+
+export { diff, formater };
