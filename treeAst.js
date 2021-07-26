3860d649f8f951de57b8521ce31737f9c4bd7b83
Ayzek <Ayzekmanduar@mail.ru>
2021-07-26 20:25:40 +0900

    renamed the module

--- treeAst.js
+++ treeAst.js
@@ -1,30 +1,30 @@
+/* eslint object-curly-newline: ["error", "never"] */
 import _ from 'lodash';
 
-/* eslint object-curly-newline: ["error", "never"] */
-const diff = (file1, file2) => {
+const createdTree = (file1, file2) => {
   const buildAst = [];
   const uniqKeys = _.sortBy(_.uniq([..._.keys(file1), ..._.keys(file2)]));
   uniqKeys.forEach((key) => {
     if (typeof file1[key] === 'object' && typeof file2[key] === 'object') {
-      buildAst.push({ name: key, children: diff(file1[key], file2[key]) });
+      buildAst.push({ name: key, children: createdTree(file1[key], file2[key]) });
     } else if (_.has(file1, key) && _.has(file2, key)) {
       if (file1[key] === file2[key]) {
         buildAst.push({ name: key, value: file1[key], status: 'unchanged' });
       } else if (typeof file1[key] === 'object') {
-        buildAst.push({ name: key, children: diff(file1[key], file1[key]), newValue: file2[key], status: 'changed' });
-      } else {
-        buildAst.push({ name: key, oldValue: file1[key], newValue: file2[key], status: 'changed' });
-      }
+        buildAst.push({ name: key, children: createdTree(file1[key], file1[key]), newValue: file2[key], status: 'changed' });
+      } else if (typeof file2[key] === 'object') {
+        buildAst.push({ name: key, children: createdTree(file2[key], file2[key]), newValue: file1[key], status: 'changed' });
+      } else buildAst.push({ name: key, oldValue: file1[key], newValue: file2[key], status: 'changed' });
     } else if (!_.has(file1, key) && _.has(file2, key)) {
       if (typeof file2[key] === 'object') {
-        buildAst.push({ name: key, children: diff(file2[key], file2[key]), status: 'added' });
+        buildAst.push({ name: key, children: createdTree(file2[key], file2[key]), status: 'added' });
       } else buildAst.push({ name: key, value: file2[key], status: 'added' });
     } else if (_.has(file1, key) && !_.has(file2, key)) {
       if (typeof file1[key] === 'object') {
-        buildAst.push({ name: key, children: diff(file1[key], file1[key]), status: 'deleted' });
+        buildAst.push({ name: key, children: createdTree(file1[key], file1[key]), status: 'deleted' });
       } else buildAst.push({ name: key, value: file1[key], status: 'deleted' });
     }
   });
   return buildAst;
 };
-export default diff;
+export default createdTree;
