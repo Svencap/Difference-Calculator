b1e8b9d3f00d482ef2b5b6f78a4ff94d057601b7
Ayzek <Ayzekmanduar@mail.ru>
2022-11-16 20:45:15 +0900

    add new files

---
+++ bin/cli-commander.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/extensions */
+
+import createdTree from '../treeAst.js';
+
+import formaterName from '../formatters/index.js';
+
+import { getPath, parser } from '../src/getPath.js';
+
+const genDiff = (filepath1, filepath2, format) => {
+  const readFileSync1 = parser(getPath(filepath1));
+  const readFileSync2 = parser(getPath(filepath2));
+  const treeAst = createdTree(readFileSync1, readFileSync2);
+  return formaterName(format, treeAst);
+};
+export default genDiff;
